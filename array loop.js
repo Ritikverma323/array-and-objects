a369510ef25862a0c471c5d944b77effc4eca511
user <iritikverma7@gmail.com>
2021-03-05 15:45:29 +0530

    first commit

---
+++ array loop.js
@@ -0,0 +1,19 @@
+let number=['1','2','3','4','5'];
+
+// for(const element in number){
+//     console.log(element);
+// }
+
+// for(const element of number){
+//     console.log(element);
+// }
+
+let person="abcde";
+
+for(const element in person){
+    console.log(element);
+}
+
+for(const element of person){
+    console.log(element);
+}
---
+++ object-loop.js
@@ -0,0 +1,17 @@
+let person ={
+    name:'max',
+    age:20
+}
+
+
+for(elements in person){
+    console.log(elements)
+
+   // element key
+}
+
+for(elements of person){
+    console.log(elements)
+
+   // person is not iterable
+}
